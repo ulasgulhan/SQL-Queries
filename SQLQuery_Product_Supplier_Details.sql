641fa74f1cec4af1eb96494eb39943f7e39cbc93
Ulaş Gülhan <55790608+ulasgulhan@users.noreply.github.com>
2023-12-26 13:30:20 +0300

    Delete SQLQuery_Product_Supplier_Details.sql

--- SQLQuery_Product_Supplier_Details.sql
+++
@@ -1,6 +0,0 @@
-select ProductName, dbo.Categories.CategoryName, dbo.Suppliers.CompanyName, dbo.Suppliers.ContactName, dbo.Suppliers.ContactTitle, 
-dbo.Suppliers.Phone, dbo.Suppliers.City 
-from Products
-left join dbo.Categories on dbo.Products.CategoryID = dbo.Categories.CategoryID
-left join dbo.Suppliers on dbo.Products.SupplierID = dbo.Suppliers.SupplierID
-where dbo.Suppliers.City = 'London'
