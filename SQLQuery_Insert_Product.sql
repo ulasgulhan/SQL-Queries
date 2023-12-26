bde3a8903df806de919b98f76a5d890c25c64393
Ulaş Gülhan <55790608+ulasgulhan@users.noreply.github.com>
2023-12-26 13:30:40 +0300

    Delete SQLQuery_Insert_Product.sql

--- SQLQuery_Insert_Product.sql
+++
@@ -1,8 +0,0 @@
-set identity_insert "Products" on
-go
-
-INSERT "Products"("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") 
-VALUES(78,'Wine',18,1,'5 boxes x 10 bags',20,55,1,7,0)
-
-set identity_insert "Products" off
-go
